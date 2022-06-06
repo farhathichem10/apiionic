efae8ee8ed709974a24bfd7ff87532197c6dcc32
Hichem Farhat <Hichem.Farhat@teriak.local>
2022-06-06 11:29:33 +0200

    aaaaazeerqsqssghhhh4

--- src/main/java/tn/teriak/controllers/MissionRest.java
+++ src/main/java/tn/teriak/controllers/MissionRest.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,5 +37,8 @@ public class MissionRest {
 
 		miss.save(m);
 	}
+	@GetMapping("/getmissionbyuserid/{id}")
+	public List<Mission> getallmissionbyuser(@PathVariable("id")int id){
+		return (List<Mission>) miss.getallMissionbyUser(id);}
 
 }
--- src/main/java/tn/teriak/repository/IMission.java
+++ src/main/java/tn/teriak/repository/IMission.java
@@ -1,11 +1,18 @@
 package tn.teriak.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 
 import tn.teriak.model.Mission;
 
 @Repository
 public interface IMission extends JpaRepository<Mission, Integer> {
+	@Query("select s from Mission s where s.user.id=:x")
+	public List <Mission> getallMissionbyUser(@Param("x")int id);
 
 }
