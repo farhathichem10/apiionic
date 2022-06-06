be4100b821418505da16c2db8e4f0a28e596d393
Hichem Farhat <Hichem.Farhat@teriak.local>
2022-06-06 10:18:25 +0200

    fffinal heroku

--- src/main/java/tn/teriak/Apispring255Application.java
+++ src/main/java/tn/teriak/Apispring255Application.java
@@ -1,8 +1,16 @@
 package tn.teriak;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import tn.teriak.model.ERole;
+import tn.teriak.model.Role;
+import tn.teriak.repository.RoleRepository;
+
+
 
 @SpringBootApplication
 public class Apispring255Application extends SpringBootServletInitializer {
@@ -10,5 +18,11 @@ public class Apispring255Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(Apispring255Application.class, args);
 	}
+	 @Bean
+	    public CommandLineRunner demoData(RoleRepository repo) {
+	        return args -> {
+	            repo.save(new Role(1, ERole.ROLE_ADMIN ));
+	        };
+	    }
 
 }
