ad82f0523e148b3ef60444e2c61c918c4b2d9732
Hichem Farhat <Hichem.Farhat@teriak.local>
2022-06-06 10:47:44 +0200

    aaaaazeerqsqssggj

--- src/main/java/tn/teriak/Apispring255Application.java
+++ src/main/java/tn/teriak/Apispring255Application.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Bean;
 
 import tn.teriak.model.ERole;
 import tn.teriak.model.Role;
+import tn.teriak.model.TypeAttestation;
+import tn.teriak.repository.ITypeAttestation;
 import tn.teriak.repository.RoleRepository;
 
 
@@ -22,6 +24,19 @@ public class Apispring255Application extends SpringBootServletInitializer {
 	    public CommandLineRunner demoData(RoleRepository repo) {
 	        return args -> {
 	            repo.save(new Role(1, ERole.ROLE_ADMIN ));
+	        };
+	    }
+	 @Bean
+	    public CommandLineRunner demoDatatypeat(ITypeAttestation repo) {
+	        return args -> {
+	            repo.save(new TypeAttestation(1,"Stage" ));
+	            repo.save(new TypeAttestation(2,"Travail" ));
+	            repo.save(new TypeAttestation(3,"Titularisation" ));
+	            repo.save(new TypeAttestation(4,"Salaire" ));
+	            repo.save(new TypeAttestation(4,"Fiche de Paie" ));
+
+
+
 	        };
 	    }
 
